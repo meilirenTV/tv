14c91c8c2ddf78705b1c64f12ece66bbfbfe2d80
meilirentv <155620301+meilirenTV@users.noreply.github.com>
2025-05-12 21:51:24 +0800

    Update tenxun.js
    Signed-off-by: meilirentv <155620301+meilirenTV@users.noreply.github.com>

--- tenxun.js
+++ tenxun.js
@@ -1,141 +1,141 @@
-var rule = {
-    title: '腾云驾雾[官]',
-    host: 'https://v.%71%71.com',
+定义变量 规则 = {
+    标题: '腾云驾雾[官]',
+    宿主: ' https://v.%71%71.com ',
     // homeUrl: '/x/bu/pagesheet/list?_all=1&append=1&channel=choice&listpage=1&offset=0&pagesize=21&iarea=-1&sort=18',
-    homeUrl: '/x/bu/pagesheet/list?_all=1&append=1&channel=cartoon&listpage=1&offset=0&pagesize=21&iarea=-1&sort=18',
-    detailUrl: 'https://node.video.%71%71.com/x/api/float_vinfo2?cid=fyid',
-    searchUrl: '/x/search/?q=**&stag=fypage',
-    searchable: 2,
-    filterable: 1,
-    multi: 1,
+    homeUrl: /x/bu/页面/列表？_全部 = 1 & 附加 = 1 & 频道 = 漫画 & 目录页面=1 & 抵消 = 0 & 页面大小 = 21 & iarea =-1 & 分类 = 18 ',
+    详细全球资源定位器(制服资源定位器): https://node . video . % 71% 71 . com/x/API/float _ vinfo 2？' cid=fyid ',
+    searchUrl: /x/s膨胀拱门扩张弓/？' q=**&stag=fypage ',
+    可通过计算机搜索的: 2,
+    可滤过的: 1,
+    多（前缀）: 1,
     // url:'/channel/fyclass?listpage=fypage&channel=fyclass&sort=18&_all=1',
-    url: '/x/bu/pagesheet/list?_all=1&append=1&channel=fyclass&listpage=1&offset=((fypage-1)*21)&pagesize=21&iarea=-1',
+    全球资源定位器(制服资源探测器): /x/卜/页面/列表？_全部 = 1 & 附加 = 1 & 频道 = 财政年度班级 & 目录页，面，张，版 = 1 & 抵消 =((财政年度页面-1)* 21)& 页面大小 = 21 & 间接寻址农村电气化管理局 =-1 ',
     // filter_url: 'sort={{fl.sort or 18}}&year={{fl.year}}&pay={{fl.pay}}',
     // filter_url: 'sort={{fl.sort or 75}}&year={{fl.year}}&pay={{fl.pay}}',
-    filter_url: 'sort={{fl.sort or 75}}&iyear={{fl.iyear}}&year={{fl.year}}&itype={{fl.type}}&ifeature={{fl.feature}}&iarea={{fl.area}}&itrailer={{fl.itrailer}}&gender={{fl.sex}}',
+    过滤器_url: 分类 = { { 股骨长度 . 分类 或者75 } } & iyear = { { fl . iyear } } & year = { { fl . year } } & I type = { { fl . type } } & I feature = { { fl . feature } } & ia rea = { { fl . area } } & itrailer = { { fl . itrailer } } & gender = { { fl . sex } } ',
     // filter: 'H4sIAAAAAAAAA+2UzUrDQBCA32XOEZLUJrGvIj0saaDBNisxBkIJCG3Fi4oepIg3EQoieqiH+vM23Zq+hRuaZLZ4ce9z2/lmd2d2+NgR+H0e+gF0DkdwFGTQgRMeJ2BAxIaSwvrqVnxcyzhlg9PttqjED2c/45cSy8DyIDcavr57q/lBw8XTd/E6qbnT8M3zTFyc72RtC/Jumd+2c8wy7KZ4nxSL5Z9uxHS+Gc+r83sWVp1eVttl4Dluk1h93YubWZVwduplAYuxoFguVp+P/y5om/Z+/YxyqfAW8pbKbeS2yi3kO/ebyE2Fy1nXXBm7DDzknspd5K7KHeSOytvI2+XAugYkKWlD2mhrM+RpSB8OmaNvTsriMEgycofc0XbHZ3HCeUTukDv67vTDQY/MIXO0zelxn5M4JI6mOPkvgswSEpgPAAA=',
-    filter: 'H4sIAAAAAAAAA+1Y3U8aWRT/X+ZZEwYEtI9207Rp0r40+9CNDxM7G8laaZCamsYEinyoDYp1sa1I/aJSK4ita3Eo9J+Ze2fmv9g7yOWc22u3xJX0RZ7gd+ee7/M7h3mujE+EQ+O6cuOP58pf+qxyQ5kOR6LKgDKlPWaoQrOrxFhmv2e0yafnj025cCFmJSouzH4E/crcQBen+VoHH/Z1cVJq2kfzHFeVuTH35FxhaFbXIqCRnJ2YjV1JI0mWnUS5I2FQ7Ur2erxDHbT9FeE+wH0Y9wLuxbgKuCDfA7gH4epIF2dfET4M+DDGg4AHMR4APIBxP+B+N2JjA0p0pi+ZCo5cnCnXHpSpP3Ut+jSig07ruEGKSz3nysoc00SSywaVy3v2LochI/TFoZXPdWBIoJNokvqLDgz5JtVTYnA/wU2SL5CF/Q4M0TW/bpFSmnsPKivvzdYWL1MQktqg69xACBXdTNi1Je4OlIaz+YHkmhxH7mdrpFrkOPJoc5W+LnEcXLJeZsFIFXyy9nPkrIFSxPGPRboY43gQ6Z1Hzw9fN98lm+9xeCb0S5kyOvvkfzTf4pHVPJD7ZvGYfFuXmk/oG9XDPqguExV7O4bOAurFTdu+B9kR2989+1FNsyMVEUF8hcby+MyDzC9ba8I9FOLMa9NYwPogwwJdtO9BVTDHRTtRpbKG/M5OkCmwR/sMcVw2RZY/YZlwZsfi1kIa34NysEvfIJsIrtZI82/xltsfqFiuG7vnxp7RIiE9OtuX1naDdM23l0rLuBaJhsNTv5JxtYiuoWwVauSl0XO2SCrJbkjrDl0v0cKhvO4c7lutrNTpJHlqNjj5DV3hOMBLAeJozMNQYCK1IetSq86bD5LRjHlpng8VlIGVonWYkwMSX2KjQVrGnMoamPKDLQfVqDh5gjjcpF6Xtyi6tU02uhMAyS+8Mw0DbUv8+WzZWknJW5rI7CiSjWWSrP93Pt0hd00OlyOHidDko/78GUJbAqaG7+brtP4M6Yvtk8zH3pmh9JlUeNugnlz7ArC3T0Tkw51xERGJFfqTAnVa87T6T8+WeAZ95FNccnxoMAAwWBIcHAEY7FY9DDUbJbO+KK+zybJg1TmjtCvmUXg83J+CEWk5FI1ooUkdxyltkFdxmj/rOU6jozelIJGNpru1FmpAQ+D37dH7PE4qsvPe7btyftuE5ezknPgriXTvPPidy/H7ELlmd6yTHeEO0wPqH/52C2DoHzvZsqtHdvqAZE5lBiYrGbNZoAWDFnmPBS6uQv/VzT1mjDV/KsWPjV5n9113mwZ8r2V+fSuVIJsQNJ+WjRZmAX5/YBpLUiLYrIVJBvxon+yw2SEPGpYDmLX4dUDzgG5npEyKbwPQnrGepDAP2/09NvcvaIIuCAgUAAA=',
-    headers: {
-        'User-Agent': 'PC_UA'
+    过滤器: H4siaaaaaaaaa+1y3u8awrt/X+泽韦蒂9207 RP 0 r 40+9 cndxm 7g 8 laazcamsyeinyodyp 1 sa 1 I/ajsk 4 ita 3 eo9j+ze 2 fmv 9g 7 yowc 22 u 3 xjx 0 rz 7 GD+ee7/m7h 3 muje+EQ+o6 cuop 58 pf+qxyq 5 kor 6 lkgd klpwaoqorxfmv 2 E0 yafnj 025 ccfmjsouzh4e/crcqcXLJeZsFIFXyy9nPkrIFSxPGPRboY43gQ6Z1Hzw9fN98lm+9xeCb0S5kyOvvkfzTf4pHVPJD7ZvGYfFuXmk/oG9XDPqguExV7O4bOAurFTdu+B9kR2989+1FNsyMVEUF8hcby+MyDzC9ba8I9FOLMa9NYwPogwwJdtO9BVTDHRTtRpbKG/M5OkCmwR/sMcVw2RZY/YZlwZsfi1kIa34NysEvfIJsIrtZI82/xltsfqFiuG7vnxp7RIiE9OtuX1naDdM23l0rLuBaJhsNTv5JxtYiuoWwVauSl0XO2SCrJbkjrDl0v0cKhvO4c7lutrNTpJHlqNjj5DV3hOMBLAeJozMNQYCK1IetSq86bD5LRjHlpng8VlIGVonWYkwMSX2KjQVrGnMoamPKDLQfVqDh5gjjcpF6Xtyi6tU02uhMAyS+8Mw0DbUv8+WzZWknJW5rI7CiSjWWSrP93Pt0hd00OlyOHidDko/78GUJbAqaG7+brtP4M6Yvtk8zH3pmh9JlUeNugnlz7ArC3T0Tkw51xERGJFfqTAnVa87T6T8+WeAZ95FNccnxoMAAwWBIcHAEY7FY9DDUbJbO+KK+zybJg1TmjtCvmUXg83J+CEWk5FI1ooUkdxyltkFdxmj/rOU6jozelIJGNpru1FmpAQ+D37dH7PE4qsvPe7btyftuE5ezknPgriXTvPPidy/H7ELlmd6yTHeEO0wPqH/52C2DoHzvZsqtHdvqAZE5lBiYrGbNZoAWDFnmPBS6uQv/VzT1mjDV/KsWPjV5n9113mwZ8r2V+fSuVIJsQNJ+WjRZmAX5/YBpLUiLYrIVJBvxon+yw2SEPGpYDmLX4dUDzgG5npEyKbwPQnrGepDAP2/09NvcvaIIuCAgUAAA=',
+    头球: {
+        '用户代理': ' PC_UA '
     },
-    timeout: 5000,
+    超时: 5000,
     // class_parse:'.site_channel a;a&&Text;a&&href;channel/(.*)',
-    cate_exclude: '会员|游戏|全部',
-    // class_name: '精选&电视剧&电影&综艺&动漫&少儿&纪录片',
-    // class_url: 'choice&tv&movie&variety&cartoon&child&doco',
-    class_name: '电影&电视剧&综艺&动漫&少儿&纪录片',
-    class_url: 'movie&tv&variety&cartoon&child&doco',
-    limit: 20,
-    play_parse: true,
-    lazy: $js.toString(() => {
-  let d = [];
-    const blockedField = 'https://123.yp22.cn/d/le/53SqFaimyxG6LrduZ';
-  try {
+' cate_exclude:'会员 游戏 全部,美食_排除: '会员|游戏|全部',
+// class_name:'精选&电视剧&电影&综艺&动漫&少儿&纪录片,// class_name: '精选&电视剧&电影&综艺&动漫&少儿&纪录片',
+// class_url:'选择&电视&电影&综艺&卡通&儿童&doco '，// class_url: 'choice&tv&movie&variety&cartoon&child&doco',
+' class_name:'电影&电视剧&综艺&动漫&少儿&纪录片,class_name: '电影&电视剧&综艺&动漫&少儿&纪录片',
+类别_url:'电影&电视&综艺&卡通&儿童&doco，class_url:'movie&tv&variety&cartoon&child&doco',
+限制:20，限制: 20,
+播放_解析: 真，播放_解析: 真实的,
+    好吧，好吧: $js.吐司！吐司(() => {
+  让 d = [];
+    常数 封闭字段 = https://123。yp22。' cn/d/le/53 sqfaimyxg 6 lrd uz ';
+  尝试 {
     // 发起请求并获取响应，添加请求头
-    let headers = {
-      'User-Agent': 'okhttp/4.12.0'
+    让 头球 = {
+      '用户代理': '确定http/4.12.0 '
       
     };
-    let responseText = request("http://150.158.112.123/%E5%85%AC%E4%BC%97%E5%8F%B7~%E7%8E%89%E7%8E%89%E5%BA%94%E7%94%A8%E7%AC%94%E8%AE%B0/jiexi.php?url=" + input, { headers: headers });
-    console.log("响应文本:", responseText); // 查看原始响应内容
+    让 响应文本 = 请求(“http://150。158 .112 .123/% E5 % 85% AC % E4 % BC % 97% E5 % 8F % B7 ~ % E7 % 8E % 89% E7 % 8E % 89% E5 % BA % 94% E7 % 94% A8 % E7 % AC % 94% E8 % AE % B0/揭西。PHP？url= " + input, { headers: headers });
+    安慰.原木("响应文本:", 响应文本); // 查看原始响应内容
 //备用http://llyh.xn--yi7aa.top/api/?key=5b317c16d457b31a3150d87c0a362a9e&url=
     // 解析 JSON 数据
-    let response = JSON.parse(responseText);
+    让 反应 = JSON.从语法上分析(响应文本);
 
     // 查找以 'url' 开头的字段
-    let urlField = Object.keys(response).find(key => key.startsWith('url'));
+    让 urlField = 目标.键(反应).发现(键 => 键.startsWith('url'));
 
     // 提取找到的字段值
-    let urlValue = urlField ? response[urlField] : null;
+    让 urlValue = urlField ? 反应[urlField] : null;
 
-    console.log("提取的随机字段值:", urlValue); // 查看提取的值
-        if (response.url.includes(blockedField)) {
-        throw new Error('该链接已被屏蔽');
+    安慰.原木("提取的随机字段值:", 关于 urlvalue); // 查看提取的值
+        如果 (反应.全球资源定位器(制服资源探测器).包含(封闭字段)) {
+        扔 新的 错误('该链接已被屏蔽');
     };
-    if (urlValue) {
+    如果 (关于 urlvalue) {
       // 处理 urlValue，或将其用于 input
-      input = {
-        url: urlValue,
-        parse: 0,
-        header: rule.headers
+      投入 = {
+        全球资源定位器(制服资源定位器): 关于 urlvalue，
+        从语法上分析: 0,
+        页眉: 规则.头球
       };
-    } else {
+    } 其他 {
       // 处理没有找到字段的情况
-      console.error("没有找到以 'url' 开头的字段");
+      安慰.错误("未找到以“网址”开始der Felder“);
     }
-  } catch (error) {
-    console.error("处理请求或数据时发生错误：", error);
+  } 捕捉 (错误) {
+    安慰.错误("处理请求或数据时发生错误：", 错误);
   }
 
-  setResult(d);
+setresult结果(四)个一个);setresult结果(D个一个);
 }),
 
     //lazy:'js:input="http:\\/\\/43.248.100.147:6068\\/KEY\\/XGJ\\/root\\/key\\/60.php?url="+input.split("?")[0];log(input);let html=JSON.parse(request(input));log(html);input=html.url||input',
-    推荐: '.list_item;img&&alt;img&&src;a&&Text;a&&data-float',
-    一级: '.list_item;img&&alt;img&&src;a&&Text;a&&data-float',
-    二级: $js.toString(() => {
-        VOD = {};
-        let d = [];
-        let video_list = [];
-        let video_lists = [];
-        let list = [];
-        let QZOutputJson;
-        let html = fetch(input, fetch_params);
-        let sourceId = /get_playsource/.test(input) ? input.match(/id=(\d*?)&/)[1] : input.split("cid=")[1];
-        let cid = sourceId;
-        let detailUrl = "https://v.%71%71.com/detail/m/" + cid + ".html";
-        log("详情页:" + detailUrl);
-        pdfh = jsp.pdfh;
-        pd = jsp.pd;
-        try {
-            let json = JSON.parse(html);
-            VOD = {
-                vod_url: input,
-                vod_name: json.c.title,
-                type_name: json.typ.join(","),
-                vod_actor: json.nam.join(","),
-                vod_year: json.c.year,
-                vod_content: json.c.description,
-                vod_remarks: json.rec,
-                vod_pic: urljoin2(input, json.c.pic)
+    推荐: 。目录_ 项目 & & 高度 & & srca & & 文本&数据浮动',
+    一级: 。目录_ 项目 & & 高度 & & srca & & 文本&数据浮动',
+    二级: 关于 关于 关于 关于 关于 关于 关于 关于 $js.在 关于Bezughof über关于ToString(() => {
+        同录像-ＯＮ- ＤＥＭＡＮＤ = {};
+        让 D  = [];
+        让 视频_列表 = [];
+        让 视频_列表 = [];
+        让 目录 = [];
+        让 在 About关于QZOutputJson;
+        让 超文本标记语言 = 取得(投入, 获取参数);
+        让 sourceid = /get_playsource/.试验(投入) ? 投入.比赛(/id=(\d*?)&/)[1] : input.split("cid=")[1];
+        让 国际开发委员会 = sourceid;
+        让 详细全球资源定位器(统一的资源定位器) = » HTTPS://v.%71%71.com/detail/m/” + cid + ".html";
+        原木("详情页:" + 详细全球资源定位器(统一资源));
+        pdfh = JSP.pdfh;
+        螺纹中径 = JSP.螺纹中径;
+        尝试 {
+            让 关于 JSONsprach = 关于 JSONsprach.从语法上分析(超文本标记语言);
+            同录像-ＯＮ- ＤＥＭＡＮＤ = {
+                视频点播网址: 投入,
+                VOD_姓名: JSON.C 个 的.标题,
+                类型名称: JSON.典型.加入(","),
+                视频点播_演员: JSON.全国制造商协会.加入(","),
+                视频点播年份: 关于 JSONsprach.C 个 的什么是.年,
+                视频点播内容: 关于 JSONsprach.C 个 的什么是.描述,
+                视频点播 _备注: JSON.娱乐场,
+                视频点播_图片: urljoin2(投入, JSON.C 的.电影)
             }
-        } catch (e) {
-            log("解析片名海报等基础信息发生错误:" + e.message)
+        } 捕捉 (于,则) {
+            原木("解析片名海报等基础信息发生错误:" + E的.消息)
         }
-        if (/get_playsource/.test(input)) {
-            eval(html);
-            let indexList = QZOutputJson.PlaylistItem.indexList;
-            indexList.forEach(function(it) {
-                let dataUrl = "https://s.video.qq.com/get_playsource?id=" + sourceId + "&plat=2&type=4&data_type=3&range=" + it + "&video_type=10&plname=qq&otype=json";
-                eval(fetch(dataUrl, fetch_params));
-                let vdata = QZOutputJson.PlaylistItem.videoPlayList;
-                vdata.forEach(function(item) {
-                    d.push({
-                        title: item.title,
-                        pic_url: item.pic,
-                        desc: item.episode_number + "\t\t\t播放量：" + item.thirdLine,
-                        url: item.playUrl
+        如果 (/get_playsource/.试验(投入)) {
+            评价评价(超文本标记语言);
+            让 索引列表 = 关于QZOutputJson.播放列表项.索引列表;
+            索引列表.为每一个(功能(它) {
+                让 数据全球资源定位器(制服资源探测器) = "https://s.video.qq.com/get_playsource？id= " + sourceId + "&plat=2&type=4&data_type=3&range=" + it + "&video_type=10&plname=qq&otype=json";
+                评价评价(取得(数据全球资源定位器(制服资源), 获取参数));
+                让 虚拟数据 = QZOutputJson.播放列表项.视频播放列表;
+                虚拟数据.为每一个(功能(项目) {
+                    d.推({
+                        标题: 项目.标题,
+                        图片_网址: 项目.电影,
+                        desc: 项目.第_集编号 + “tttt播放量:" + 项目.第三线,
+                        全球资源定位器(制服资源定位器): 项目.playUrl
                     })
                 });
-                video_lists = video_lists.concat(vdata)
+                视频_列表 = 视频_列表.串联(虚拟数据)
             })
-        } else {
-            let json = JSON.parse(html);
-            video_lists = json.c.video_ids;
-            let url = "https://v.qq.com/x/cover/" + sourceId + ".html";
-            if (video_lists.length === 1) {
-                let vid = video_lists[0];
-                url = "https://v.qq.com/x/cover/" + cid + "/" + vid + ".html";
-                d.push({
-                    title: "在线播放",
-                    url: url
+        } 其他 {
+            让 json说的 = JSON说的.从语法上分析(超文本标记语言);
+            视频_列表 = json说的.c是什么.视频_ 本能冲动;
+            让 全球资源定位器(制服资源探测器) = “https://v.qq.com/x/cover/" + sourceId + ".html";
+            如果 (视频_列表.长度 === 1) {
+                让 录像的简称 = 视频_列表[0];
+                全球资源定位器(制服资源探测器) = “https://v.qq.com/x/cover/" + 国际开发委员会 + "/" + vid + ".html";
+                d.推({
+                    标题: "在线播放",
+                    全球资源定位器(制服资源定位器): 全球资源定位器(制服资源 探测器)
                 })
-            } else if (video_lists.length > 1) {
-                for (let i = 0; i < video_lists.length; i += 30) {
-                    video_list.push(video_lists.slice(i, i + 30))
+            } 其他 如果 (视频_列表.长度 > 1) {
+                为 (让 我 = 0; 我 < 视频_列表.长度; 我 += 30) {
+                    视频_列表.推(视频_列表.薄片(我, 我 + 30))
                 }
                 video_list.forEach(function(it, idex) {
-                    let o_url = "https://union.video.qq.com/fcgi-bin/data?otype=json&tid=1804&appid=20001238&appkey=6c03bbe9658448a4&union_platform=1&idlist=" + it.join(",");
+                    让 o_url = "https://union.video.qq.com/fcgi-bin/data？otype = JSON & tid = 1804 & appid = 20001238 & appkey = 6 c 03 bb 9658448 a4 & union _ platform = 1 & id list = " + it.join(",");
                     let o_html = fetch(o_url, fetch_params);
                     eval(o_html);
                     QZOutputJson.results.forEach(function(it1) {
@@ -144,58 +144,58 @@ var rule = {
                         d.push({
                             title: it1.title,
                             pic_url: it1.pic160x90.replace("/160", ""),
-                            desc: it1.video_checkup_time,
-                            url: url,
-                            type: it1.category_map && it1.category_map.length > 1 ? it1.category_map[1] : ""
+                            desc: it1.视频_检查_时间,
+                            全球资源定位器(Uniform Resource Locator): 全球资源定位器(Uniform Resource Locator),
+                            类型: it1.类别_地图 && it1.category_map.length > 1 ? it1.category_map[1] : ""
                         })
                     })
                 })
             }
         }
-        let yg = d.filter(function(it) {
-            return it.type && it.type !== "正片"
+        让 yg = d.过滤器(功能(它) {
+            返回 它.类型 && 它.类型 !== "正片"
         });
-        let zp = d.filter(function(it) {
-            return !(it.type && it.type !== "正片")
+        让 零功率 = d.过滤器(功能(它) {
+            返回 !(它.类型 && 它.类型 !== "正片")
         });
-        VOD.vod_play_from = yg.length < 1 ? "四季快云" : "四季快云$$$预告及花絮";
-        VOD.vod_play_url = yg.length < 1 ? d.map(function(it) {
-            return it.title + "$" + it.url
-        }).join("#") : [zp, yg].map(function(it) {
-            return it.map(function(its) {
-                return its.title + "$" + its.url
-            }).join("#")
-        }).join("$$$");
+        同录像-ＯＮ- ＤＥＭＡＮＤ.视频点播_播放_自 = yg.长度 < 1 ? "魅力人4K专线" : "4K专线$$$预告及花絮";
+        同录像-ＯＮ- ＤＥＭＡＮＤ.视频点播_播放_网址 = yg.长度 < 1 ? d.map(function(it) {
+            返回 它.标题 + "$" + 它.全球资源定位器(制服资源 探测器)
+        }).加入("#") : [零功率, yg].地图(功能(它) {
+            返回 它.地图(功能(它的) {
+                返回 它的.标题 + "$" + 它的.全球资源定位器(制服资源 探测器)
+            }).加入("#")
+        }).加入("$$$");
     }),
     搜索: $js.toString(() => {
-        let d = [];
+        让 d = [];
         pdfa = jsp.pdfa;
         pdfh = jsp.pdfh;
-        pd = jsp.pd;
-        let html = request(input);
-        let baseList = pdfa(html, "body&&.result_item_v");
-        log(baseList.length);
-        baseList.forEach(function(it) {
-            let longText = pdfh(it, ".result_title&&a&&Text");
-            let shortText = pdfh(it, ".type&&Text");
-            let fromTag = pdfh(it, ".result_source&&Text");
-            let score = pdfh(it, ".figure_info&&Text");
-            let content = pdfh(it, ".desc_text&&Text");
+        螺纹中径 = jsp.螺纹中径;
+        让 超文本标记语言 = 请求(投入);
+        让 基线列表 = pdfa(超文本标记语言, “身体&&。结果_项目_v");
+        原木(基线列表.长度);
+        基线列表.为每一个(功能(它) {
+            让 长文本 = pdfh(它, ".结果标题& & a & &文本");
+            让 短文本 = pdfh(它, ".键入和文本");
+            让 fromTag = pdfh(它, ".结果来源和文本");
+            让 得分 = pdfh(它, ".figure _ info & & Text ");
+            让 内容 = pdfh(它, ".desc _文本& &文本");
             // let url = pdfh(it, ".result_title&&a&&href");
-            let url = pdfh(it, "div&&r-data");
+            让 全球资源定位器(制服资源 探测器) = pdfh(它, "分区和r数据");
             // log(longText);
             // log(shortText);
             // log('url:'+url);
-            let img = pd(it, ".figure_pic&&src");
-            url = "https://node.video.qq.com/x/api/float_vinfo2?cid=" + url.match(/.*\/(.*?)\.html/)[1];
-            log(shortText + "|" + url);
-            if (fromTag.match(/腾讯/)) {
-                d.push({
-                    title: longText.split(shortText)[0],
+            让 img = 螺纹中径(它, ".图_pic&&src ");
+            全球资源定位器(制服资源探测器) = "https://node.video.qq.com/x/api/float_vinfo2？cid= " + url.match(/.*\/(.*?)\.html/)[1];
+            原木(短文本 + "|" + 全球资源定位器(制服资源 探测器));
+            如果 (fromTag.比赛(/腾讯/)) {
+                d.推({
+                    标题: 长文本.使分离(短文本)[0],
                     img: img,
-                    url: url,
-                    content: content,
-                    desc: shortText + " " + score
+                    全球资源定位器(制服资源定位器): 全球资源定位器(制服资源 探测器),
+                    内容: 内容,
+                    desc: 短文本 + " " + 得分
                 })
             }
         });
